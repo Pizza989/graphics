90f9f4551d5d5fd272221b526cb1083455af4b04
Noah Simai <noah@Noahlaptop.fritz.box>
2025-05-07 20:39:47 +0200

    render

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,6 +1,6 @@
 use euclid::{Point2D, Rect as Rectangle, Size2D};
 
 pub struct ScreenSpace;
-pub type Size = Size2D<i32, ScreenSpace>;
-pub type Rect = Rectangle<i32, ScreenSpace>;
-pub type Point = Point2D<i32, ScreenSpace>;
+pub type Size = Size2D<u32, ScreenSpace>;
+pub type Rect = Rectangle<u32, ScreenSpace>;
+pub type Point = Point2D<u32, ScreenSpace>;
--- src/renderer.rs
+++ src/renderer.rs
@@ -1,8 +1,81 @@
+use std::any::Any;
+
+use wgpu::Origin3d;
+
 use crate::{
     geometry::Size,
-    infrastructure::element_tree::{ElementTree, PlacedElementTree},
+    infrastructure::{
+        element::PlacedElement,
+        element_tree::{ElementTree, PlacedElementTree},
+        widget::PlacedWidget,
+    },
 };
 
 impl PlacedElementTree<wgpu::Texture> {
-    pub fn render(&self) {}
+    pub fn render(&self, device: &wgpu::Device, encoder: &mut wgpu::CommandEncoder) {
+        let destination = device.create_texture(&wgpu::TextureDescriptor {
+            label: Some("Render Destination"),
+            size: wgpu::Extent3d {
+                width: self.root.rect.width(),
+                height: self.root.rect.height(),
+                depth_or_array_layers: 1,
+            },
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            usage: wgpu::TextureUsages::COPY_DST | wgpu::TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[wgpu::TextureFormat::Rgba8UnormSrgb],
+        });
+
+        self.root
+            .children
+            .iter()
+            .map(|element| PlacedElementTree::traverse(element, encoder, &destination));
+    }
+
+    fn traverse(
+        element: &PlacedElement<wgpu::Texture>,
+        encoder: &mut wgpu::CommandEncoder,
+        destination: &wgpu::Texture,
+    ) {
+        match element {
+            PlacedElement::Widget(placed_widget) => {
+                PlacedElementTree::copy(encoder, placed_widget, destination)
+            }
+            PlacedElement::Layout(placed_layout) => placed_layout
+                .children
+                .iter()
+                .map(|element| PlacedElementTree::traverse(element, encoder, destination))
+                .collect(),
+        };
+    }
+
+    // Panics:
+    // When the PlacedWidget's rect's size exceeds the associated texture for the source
+    fn copy(
+        encoder: &mut wgpu::CommandEncoder,
+        source: &PlacedWidget<dyn Any, wgpu::Texture>,
+        destination: &wgpu::Texture,
+    ) {
+        encoder.copy_texture_to_texture(
+            wgpu::TexelCopyTextureInfoBase {
+                texture: &source.widget.texture(),
+                mip_level: 1,
+                origin: Origin3d::default(),
+                aspect: wgpu::TextureAspect::All,
+            },
+            wgpu::TexelCopyTextureInfoBase {
+                texture: destination,
+                mip_level: 1,
+                origin: Origin3d::default(),
+                aspect: wgpu::TextureAspect::All,
+            },
+            wgpu::Extent3d {
+                width: source.rect.width(),
+                height: source.rect.height(),
+                depth_or_array_layers: 1,
+            },
+        );
+    }
 }
